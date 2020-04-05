28a8e73ecef3ef0ad4a257932e36a49c3a95cdfa
Khoi <hakhoipham@gmail.com>
2020-04-05 18:13:13 -0400

    renamed items page to Shop

--- components/Nav.js
+++ components/Nav.js
@@ -5,7 +5,7 @@ import NavStyles from '../components/styles/NavStyles';
 const Nav = () => (
   <NavStyles>
     <Link href="/items">
-      <a>Items</a>
+      <a>Shop</a>
     </Link>
     <Link href="/sell">
       <a>Sell</a>
