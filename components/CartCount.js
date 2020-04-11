60d1f332025ac7657b6cab450e462f359ad0f275
Khoi <hakhoipham@gmail.com>
2020-04-11 19:39:39 -0400

    add cart total count tracker in the navigation bar

---
+++ components/CartCount.js
@@ -0,0 +1,62 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+const Dot = styled.div`
+  background: ${props => props.theme.red};
+  color: white;
+  border-radius: 50%;
+  padding: 0.5rem;
+  line-height: 2rem;
+  min-width: 3rem;
+  margin-left: 1rem;
+  font-weight: 100;
+
+  /* keep character size of each number constant
+  (e.g. skinny '1' will keep same width as larger '2) */
+  font-feature-settings: 'tnum';
+  font-variant-numeric: tabular-nums;
+`;
+
+const AnimationStyles = styled.span`
+  position: relative;
+  .count {
+    display: block;
+    position: relative;
+    transition: all 0.4s;
+    backface-visibility: hidden;
+  }
+  /* Initial state of the entered Dot (flip on its back) */
+  .count-enter {
+    /* 0.5turn = 180 degrees */
+    transform: scale(4) rotateX(0.5turn);
+  }
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+  .count-exit {
+    position: absolute;
+    top: 0;
+    transform: rotateX(0);
+  }
+  .count-exit-active {
+    transform: scale(4) rotateX(0.5turn);
+  }
+`;
+
+const CartCount = ({ count }) => (
+  <AnimationStyles>
+    <TransitionGroup>
+      <CSSTransition
+        unmountOnExit
+        className="count"
+        classNames="count"
+        key={count}
+        timeout={{ enter: 400, exit: 400 }}>
+        <Dot>{count}</Dot>
+      </CSSTransition>
+    </TransitionGroup>
+  </AnimationStyles>
+);
+
+export default CartCount;
--- components/Nav.js
+++ components/Nav.js
@@ -4,6 +4,7 @@ import { Mutation } from 'react-apollo';
 import NavStyles from '../components/styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import CartCount from './CartCount';
 import { TOGGLE_CART_MUTATION } from './Cart';
 
 const Nav = () => (
@@ -27,7 +28,12 @@ const Nav = () => (
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
               {toggleCart => (
-                <button onClick={toggleCart}> My Cart</button>
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount count={me.cart.reduce(
+                    (tally, cartItem) => tally + cartItem.quantity, 0
+                  )} />
+                </button>
               )}
             </Mutation>
           </>
