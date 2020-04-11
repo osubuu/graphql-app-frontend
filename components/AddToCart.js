ed10c3ce4843d5dc350ec9f13456be47e8db602e
Khoi <hakhoipham@gmail.com>
2020-04-11 18:51:35 -0400

    display each cart item information
    - CartItem component added
    - additional response info feteched from current user query
    - cart total calculation added
    - added UX disabling of add to cart button when adding

--- components/AddToCart.js
+++ components/AddToCart.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
+import { CURRENT_USER_QUERY } from './User';
+
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
     addToCart(id: $id) {
@@ -15,9 +17,14 @@ class AddToCart extends Component {
   render() {
     const { id } = this.props;
     return (
-      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
-        {addToCart => (
-          <button onClick={addToCart}>Add To Cart 🛒</button>
+      <Mutation
+        mutation={ADD_TO_CART_MUTATION}
+        variables={{ id }}
+        refetchQueries={[{
+          query: CURRENT_USER_QUERY
+        }]}>
+        {(addToCart, { loading }) => (
+          <button disabled={loading} onClick={addToCart}>Add{loading && 'ing'} To Cart 🛒</button>
         )}
       </Mutation>
     );
