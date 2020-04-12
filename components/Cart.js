7a5e2973f3ff94d300e4de01dad61e51eb454ec1
Khoi <hakhoipham@gmail.com>
2020-04-12 12:34:51 -0400

    make render props component to clean up nested render props logic

--- components/Cart.js
+++ components/Cart.js
@@ -1,5 +1,6 @@
 import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { adopt } from 'react-adopt';
 
 import User from './User';
 import CartItem from './CartItem';
@@ -22,41 +23,41 @@ const TOGGLE_CART_MUTATION = gql`
   }
 `;
 
+// make a render prop component to clean up nested render prop logic
+const Composed = adopt({
+  user: ({ render }) => <User>{render}</User>,
+  toggleCart: ({ render }) => <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>,
+  localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>,
+})
+
 const Cart = props => {
   return (
-    <User>
-      {({ data: { me } }) => {
+    <Composed>
+      {({ user, toggleCart, localState }) => {
+        const { me } = user.data;
         if (!me) return null;
         return (
-          <Mutation mutation={TOGGLE_CART_MUTATION}>
-            {toggleCart => (
-              <Query query={LOCAL_STATE_QUERY}>
-                {({ data }) => (
-                  <CartStyles open={data.cartOpen}>
-                    <header>
-                      <CloseButton
-                        title="close"
-                        onClick={toggleCart}>
-                        &times;
+          <CartStyles open={localState.data.cartOpen}>
+            <header>
+              <CloseButton
+                title="close"
+                onClick={toggleCart}>
+                &times;
                       </CloseButton>
-                      <Supreme>{me.name}'s Cart</Supreme>
-                      <p>You have {me.cart.length} item{me.cart.length === 1 ? '' : 's'} in your cart.</p>
-                    </header>
-                    <ul>
-                      {me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)}
-                    </ul>
-                    <footer>
-                      <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                      <SickButton>Checkout</SickButton>
-                    </footer>
-                  </CartStyles>
-                )}
-              </Query>
-            )}
-          </Mutation>
+              <Supreme>{me.name}'s Cart</Supreme>
+              <p>You have {me.cart.length} item{me.cart.length === 1 ? '' : 's'} in your cart.</p>
+            </header>
+            <ul>
+              {me.cart.map(cartItem => <CartItem key={cartItem.id} cartItem={cartItem} />)}
+            </ul>
+            <footer>
+              <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+              <SickButton>Checkout</SickButton>
+            </footer>
+          </CartStyles>
         )
       }}
-    </User>
+    </Composed>
   )
 };
 
