2c1fda12ea93b80595edc68501ed474ae018f0bb
Khoi <hakhoipham@gmail.com>
2020-04-18 12:36:13 -0400

    started unit tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16'; // different module for every version of react
 
 configure({ adapter: new Adapter() });
