9ada73b7b60d8d26b183b3839844eb0e30c27267
Khoi <hakhoipham@gmail.com>
2020-04-19 15:03:16 -0400

    added tests for Order component

--- components/Order.js
+++ components/Order.js
@@ -7,7 +7,7 @@ import formatMoney from '../lib/formatMoney';
 import ErrorMessage from './ErrorMessage';
 import OrderStyles from './styles/OrderStyles';
 
-const SINGLE_ORDER_QUERY = gql`
+export const SINGLE_ORDER_QUERY = gql`
   query SINGLE_ORDER_QUERY($id: ID!) {
     order(id: $id) {
       id
@@ -36,7 +36,7 @@ const Order = props => (
       if (loading) return <p>Loading...</p>;
       const { order } = data;
       return (
-        <OrderStyles>
+        <OrderStyles data-test="order">
           <Head>
             <title>Sick Fits - Order {order.id}</title>
           </Head>
