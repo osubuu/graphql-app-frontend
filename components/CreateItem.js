a2c09d63ff3771515b36398fbc8177af394b1efa
Khoi <hakhoipham@gmail.com>
2020-05-10 10:19:43 -0400

    use largeImage state for the mutation params

--- components/CreateItem.js
+++ components/CreateItem.js
@@ -32,7 +32,7 @@ class CreateItem extends Component {
     title: '',
     description: '',
     image: '',
-    // largeImage: '',
+    largeImage: '',
     price: 0,
   }
 
@@ -56,7 +56,7 @@ class CreateItem extends Component {
     const file = await res.json();
     this.setState({
       image: file.secure_url,
-      // largeImage: file.eager[0].secure_url,
+      largeImage: file.eager[0].secure_url,
     });
   }
 
